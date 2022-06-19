892eea785a0a2e25ba80cc418dcb4482cf0d89ba
Amin Ahmady <aminahmadydeveloper@gmail.com>
2022-06-19 11:22:40 +0430

    Auth Fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,6 @@ import NextAuth from "next-auth"
 import FacebookProvider from "next-auth/providers/facebook"
 
 export default NextAuth({
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-  url: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   providers: [
     FacebookProvider({
         clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
